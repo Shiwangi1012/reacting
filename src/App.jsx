45a13794b0cfc26aed224b85d54d2c939766acc0
Shiwangi Sejal <shiwangisejal@gmail.com>
2025-06-28 22:31:31 +0530

    kio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React , {useState} from 'react'
 import Card from "./Components/Card";
 import Music from "./Components/Music";
 
-function App(){
+
+function App() {
+  const [val,setVal] = useState({name : "adi" , banned : false});
+
   return (
-    <>
-    <Music/>
-    </>
+    <div className='p-5'>
+        <h3>name : {val.name}</h3>
+        <h3>banned: {val.banned.toString()}</h3>
+        <button onClick={()=>setVal({...val , banned : !val.banned})} className={`px-3 py-1 ${val.banned ? "bg-red-600" : "bg-blue-500"} mt-2 rounded-full `}>Click to change</button>
+    </div>
   )
 }
 
-export default App;
+
+export default App
